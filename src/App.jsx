dabd9f427bad00d9f3cb6699dd6446b093073063
Favour Afolayan <favourafolayan@gmail.com>
2019-04-29 17:30:49 +0100

    [chore] setup empty react project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+/**
+ * Class App
+ * @description Application base component
+ */
+export default class App extends Component {
+  /**
+   * @returns {Function} render()
+   */
+  render() {
+    return (
+      <div>
+        <h1> Hello, World! </h1>
+        <p>We are here baby</p>
+      </div>
+    );
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  mode: 'development',
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+      },
+      {
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './public/index.html',
+      filename: './index.html',
+    }),
+  ],
+};
