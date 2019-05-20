36cde66e9eea99f9e4570ba88bd89f809e172430
Favour Afolayan <favourafolayan@gmail.com>
2019-05-20 05:41:46 +0100

    [finishes] 166103797 setup react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1> Hello, World! </h1>
-        <p>We are here baby</p>
-      </div>
-    );
-  }
-}
+//components
+import Signup from './components/containers/Signup/Signup';
+import Login from './components/containers/Login/Login';
+
+const App = () => {
+  return (
+    <Switch>
+      <Route exact path="/" component={Signup} />
+      <Route exact path="/signup" component={Signup} />
+      <Route exact path="/login" component={Login} />
+    </Switch>
+  );
+};
+
+export default App;
---
+++ src/components/containers/Signup/Signup.jsx
@@ -0,0 +1,10 @@
+import React, { Component, Fragment } from 'react';
+import { Link } from 'react-router-dom';
+
+class Signup extends Component {
+  render() {
+    return <p>Welcome to the sign up page</p>;
+  }
+}
+
+export default Signup;
