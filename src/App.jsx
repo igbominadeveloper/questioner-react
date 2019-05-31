65d82e6c79b1590126aeed965a1bb10697c80c69
Favour Afolayan <favourafolayan@gmail.com>
2019-05-31 08:28:24 +0100

    [ft add hocs for guest and auth checks

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,16 @@ import Login from './components/containers/Login/Login';
 import Homepage from './pages/Landing';
 import NewMeetup from './components/containers/NewMeetup/NewMeetup';
 import SingleMeetup from './pages/SingleMeetup/SingleMeetup';
+import AdminWrapper from './components/containers/HOC/Authorization/AdminWrapper';
+import GuestWrapper from './components/containers/HOC/Authorization/GuestWrapper';
 
 const App = () => {
   return (
     <Switch>
       <Route exact path="/" component={Homepage} />
-      <Route path="/signup" component={Signup} />
-      <Route path="/login" component={Login} />
-      <Route path="/organize" component={NewMeetup} />
+      <GuestWrapper path="/signup" component={Signup} />
+      <GuestWrapper path="/login" component={Login} />
+      <AdminWrapper path="/organize" component={NewMeetup} />
       <Route path="/meetup=:meetupId" component={SingleMeetup} />
     </Switch>
   );
